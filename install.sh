df57c44d64989d9a09a2a3248fcbbfb3e4aa92f0
Milan Kotarac <kotaracmilan@hotmail.com>
2022-10-23 22:25:43 +0200

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #! usr/bin/bash
 
-mkdir /srv/docker
-cd /srv/docker
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-apt-get install git
-git clone https://github.com/kotaracmilan/java-developer-environment.git
 docker-compose up -d
 
 
