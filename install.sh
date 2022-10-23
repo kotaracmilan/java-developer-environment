2f4b9b9f9c8aa1255746a39e2667151b71e91d20
Milan Kotarac <kotaracmilan@hotmail.com>
2022-10-23 14:47:10 +0200

    add sonarqube

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 
 mkdir /srv/docker
 cd /srv/docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 apt-get install git
-git clone ______
+git clone https://github.com/kotaracmilan/java-developer-environment.git
 docker-compose up -d
 
 
