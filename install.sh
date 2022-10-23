2003b86ded24fa6b72381dd6168b6556df89fcf1
Milan Kotarac <kotaracmilan@hotmail.com>
2022-10-24 00:18:09 +0200

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #! usr/bin/bash
+echo "Wellcome to installer! Please insert domain name:"
+read domainname
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 docker network create votek-solutions-dev-network
+apt-get install snapd
+snap install --classic certbot
+uwf allow http
+uwf allow https
+certbot certonly --standalone  -d *.$domainname
 docker-compose up -d
 
 
